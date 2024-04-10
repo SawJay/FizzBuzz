ebd6f190edd5b4f61f8e45a4bd8562089f7da56e
SawJay <sawyerjd1@gmail.com>
2024-04-10 18:02:09 -0400

    Started FizzBuzz

--- js/site.js
+++ js/site.js
@@ -1,12 +1,80 @@
-// Display the message the user entered. 
-function displayMessage() {
-    let inputBox = document.getElementById('message');
-    let message = inputBox.value;
+// Collect user values
+function getValues() {
+  let startNumber = document.getElementById('startNumber').value;
+  let endNumber = document.getElementById('endNumber').value;
 
+  startNumber = Number(startNumber)
+  endNumber = Number(endNumber)
+
+  if (isNaN(startNumber) || isNaN(endNumber)) {
     Swal.fire({
-        title: "App Name",
-        text: message,
-        icon: "success",
-        backdrop: false
-      });
+      icon: 'error',
+      title: 'Oops!',
+      text: 'Please enter valid numbers for CountUP! to use.',
+      backdrop: false
+    })
+  } else if ((startNumber > endNumber) || (startNumber < 0) || (endNumber > 100)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops!',
+      text: 'Please enter valid numbers for CountUP! to use.',
+      backdrop: false
+    })
+  } else {
+
+    let generatedNumbers = generateFizzBuzz(startNumber, endNumber);
+
+    displayFizzBuzz(generatedNumbers);
+  }
+
+}
+
+function generateFizzBuzz(fizzValue, buzzValue) {
+  let fizzbuzzResults = [];
+
+  for (let index = 1; index <= 100; index += 1) {
+
+    let message;
+
+    if (index % fizzValue == 0 && index % buzzValue == 0) {
+      message = 'FizzBuzz';
+    }
+
+    else if (index % fizzValue == 0) {
+      message = 'Fizz';
+    }
+
+    else if (index % buzzValue == 0) {
+      message = 'Buzz';
+    }
+
+    else {
+      message = index;
+    }
+
+    fizzbuzzResults.push(message);
+
+  }
+
+  return fizzbuzzResults;
+
+}
+
+function displayFizzBuzz(fizzbuzzResults) {
+
+  let tableHtml = '';
+
+  for (let index = 0; index < fizzbuzzResults.length; index += 1) {
+
+    let message = fizzbuzzResults[index];
+
+    let className = '';
+
+    tableHtml += `<tr><td class="${className}">${message}</td></tr>`;
+
+  }
+
+  let tbody = document.getElementById('results');
+  tbody.innerHTML = tableHtml;
+
 }
